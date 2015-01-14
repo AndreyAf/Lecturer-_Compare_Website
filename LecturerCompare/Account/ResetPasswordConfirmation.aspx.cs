e6308f0811c9f6900f764594b6f64ec5f8648468
AndreyAf <mushushushu@gmail.com>
2015-01-14 23:52:55 +0200

    INIT

---
+++ LecturerCompare/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace LecturerCompare.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ LecturerCompare/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LecturerCompare.Startup))]
+namespace LecturerCompare
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
